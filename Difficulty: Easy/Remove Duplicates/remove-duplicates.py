24bb7e0d02d444587bb8ca0cb648eef8967d8103
Vaibhav chauhan <117555569+vaibhav639@users.noreply.github.com>
2024-07-31 17:47:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates/remove-duplicates.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+class Solution:
+	def removeDups(self, str):
+		uniqueString = ""
+		
+		for char in str:
+		    if char in uniqueString:
+		        pass
+		    else:
+		        uniqueString += char
+		return uniqueString
+		        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        s = input()
+
+        ob = Solution()
+        answer = ob.removeDups(s)
+
+        print(answer)
+
+# } Driver Code Ends
