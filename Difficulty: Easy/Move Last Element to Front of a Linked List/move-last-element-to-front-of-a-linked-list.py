e829cb726a6bd79538c46a485ab3aec98229e73c
Vaibhav chauhan <117555569+vaibhav639@users.noreply.github.com>
2024-08-02 20:22:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move Last Element to Front of a Linked List/move-last-element-to-front-of-a-linked-list.py
@@ -0,0 +1,76 @@
+from typing import Optional
+
+"""
+
+Definition for singly Link List Node
+class Node:
+    def __init__(self,x):
+        self.data=x
+        self.next=None
+
+You can also use the following for printing the link list.
+printList(node)
+"""
+
+class Solution:
+    def moveToFront(self, head : Optional['Node']) -> Optional['Node']:
+        current = head
+        second_last = None
+        
+        if not head or head.next is None:
+            return head
+        
+        while current.next:
+            second_last = current
+            current = current.next
+            
+        second_last.next = None
+        current.next = head 
+        
+        head = current
+        
+        return head
+            
+                
+                
+            
+                
+        
+
+
+
+#{ 
+ # Driver Code Starts
+class Node:
+    def __init__(self,x):
+        self.data=x
+        self.next=None
+
+def printList(node):
+    while (node != None):
+        print(node.data,end=" ")
+        node = node.next
+    print()
+def inputList():
+    n=int(input())#lenght of link list
+    data=[int(i) for i in input().strip().split()]#all data of linked list in a line
+    head = Node(data[0])
+    tail = head;
+    for i in range(1,n):
+        tail.next = Node(data[i]);
+        tail = tail.next;
+    return head;
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        head = inputList()
+        
+        obj = Solution()
+        res = obj.moveToFront(head)
+        
+        printList(res)
+        
+
+# } Driver Code Ends
