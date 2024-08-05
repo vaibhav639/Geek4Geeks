932a0c5da06b821e00cae514c29c49d7314c8c11
Vaibhav chauhan <117555569+vaibhav639@users.noreply.github.com>
2024-08-05 16:57:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.py
@@ -0,0 +1,110 @@
+#User function Template for python3
+'''
+	Your task is to check if given linkedlist
+	is a pallindrome or not.
+	
+	Function Arguments: head (reference to head of the linked list)
+	Return Type: boolean , no need to print just return True or False.
+
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+
+	Contributed By: Nagendra Jha
+'''
+#Function to check whether the list is palindrome.
+class Solution:
+    def isPalindrome(self, head):
+        if head is None or head.next is None:
+            return True
+            
+        slow = head
+        fast = head
+        
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+        
+        
+        def reverseList(head):
+            previous = None
+            current = head
+            while current:
+                next_node = current.next
+                current.next = previous
+                previous = current
+                current = next_node
+            return previous
+        
+        second_half = reverseList(slow)
+        first_half = head
+        
+        while second_half:
+            if second_half.data == first_half.data:
+                pass
+            else:
+                return False
+                break
+            second_half = second_half.next
+            first_half = first_half.next
+            
+        return True
+                
+            
+        
+            
+
+
+#{ 
+ # Driver Code Starts
+#main
+
+
+class Node:
+    # Constructor to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+
+    # Function to insert a new node at the beginning
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # Utility function to prit the linked LinkedList
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            temp = temp.next
+        print("")
+
+
+if __name__ == '__main__':
+    t = int(input())
+    while (t > 0):
+        llist = LinkedList()
+        values = input().strip().split()
+        for i in reversed(values):
+            llist.push(i)
+        flag = Solution().isPalindrome(llist.head)
+        if flag:
+            print("true")
+        else:
+            print("false")
+        t -= 1
+# Contributed by: Harshit Sidhwa
+
+# } Driver Code Ends
