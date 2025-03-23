01b3e64f83206d5b87d69fa4458cb63b279ee520
Vaibhav chauhan <117555569+vaibhav639@users.noreply.github.com>
2025-03-23 17:42:05 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.py
@@ -0,0 +1,55 @@
+#User function Template for python3
+
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction. 
+    def rotateArr(self, arr, d):
+        n = len(arr)
+        d = d % n
+        def reverse(start,end):
+            while start < end:
+                arr[start],arr[end] = arr[end],arr[start]
+                start+=1
+                end-=1
+    
+        reverse(0,d-1)
+        reverse(d,n-1)
+        reverse(0,n-1)
+        
+        
+            
+        
+            
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+    T = int(input())
+
+    while (T > 0):
+        A = [int(x) for x in input().strip().split()]
+        nd = [int(x) for x in input().strip().split()]
+        D = nd[0]
+        ob = Solution()
+        ob.rotateArr(A, D)
+
+        for i in A:
+            print(i, end=" ")
+
+        print()
+
+        T -= 1
+
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
